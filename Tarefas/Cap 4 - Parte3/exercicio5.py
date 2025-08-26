42fab2c3fdff7e5a378c5e5b7a141edd05119a7a
luizOtpaiva <luizotavio.paiva04@gmail.com>
2025-08-26 16:35:02 -0300

    adicionando dever parte3 Cap4

--- Tarefas/Cap 4 - Parte3/exercicio5.py
+++ Tarefas/Cap 4 - Parte3/exercicio5.py
@@ -12,9 +12,16 @@ def main():
     # loop para ver quantas vezes cada empresa aparece
 
     arr_empresa = ds[1:, 1]  # do índice 1 até o fim , só a coluna 1
-    print("Coluna de empresa:\n", arr_empresa)  # Exibe a coluna de empresa 
+    print("Coluna de empresa:\n", arr_empresa)  # Exibe a coluna de empresa
 
+    achando_spaceX = np.char.find(arr_empresa, "SpaceX")
+    print(achando_spaceX)
 
+    #para contar as missões da SpaceX
+    quantidade_de_vezes = np.sum(arr_empresa == "SpaceX")
+    
+    print("\nQuantidade de missões da SpaceX:")
+    print(quantidade_de_vezes)
 
 if __name__ == '__main__':  
     main() 
